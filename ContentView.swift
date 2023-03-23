b5645390b37c0468e2af6d394626ca095f7a2466
Cynthia Elvira <105707079+CynthiaElvira@users.noreply.github.com>
2023-03-23 15:33:29 +0800

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Cynthia Elvira
+//
+//  Created by HC-101 on 2023/3/18.
+//  Copyright © 2023 Creative. All rights reserved.
+//
+
+import SwiftUI
+import Cocoa
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+var _var = "Swift"
+var VarT:Int = 100
+var VarC:Int = 200
+var Dragon = "Tohru"
